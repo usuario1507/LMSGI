3574299e10e7a4aecb280b87259e8d6768ac38b9
usuario1507 <angelgarcialopez15@gmail.com>
2021-12-08 17:07:33 +0100

    html formacion.html home.html servicios.html trabajos.html js app.js

---
+++ HTML/RWD/2021/Maqueta2/js/app.js
@@ -0,0 +1,11 @@
+
+
+// carga dinámica de pÁGINAS
+function inicio() {
+    console.log('documento app cargado');
+    console.log(document);
+    const vmain = document.querySelector('main');
+    //vmain.innerHTML = '<h1>Hola Caracola</h1>';
+    
+
+}
