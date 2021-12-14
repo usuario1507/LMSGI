33402fec36c11024af8eb714d4b9c45c5e0de4bd
usuario1507 <angelgarcialopez15@gmail.com>
2021-12-14 18:03:25 +0100

    Inmobiliarias ÁNGEL (Práctica obligatoria 1º trimestre) css estilos.css html Formacion.html Ofertas.html Quienessomos.html Servicios.html img Casa-Alhabia.png Casa-El Ejido.png Chalet-Carboneras.png Chalet-Los Gallardos.png InmobiliariasÁNGEL.png Local-Níjar.png Piso-Níjar.png Piso-Vera.png Vivienda_RoquetasdeMar.png Vivienda-Cuevasdealmanzora.png js app.js index.html

---
+++ HTML/flexbox/Inmobiliarias ÁNGEL (Práctica obligatoria 1º trimestre)/js/app.js
@@ -0,0 +1,11 @@
+
+
+// Carga dinámica de páginas
+function inicio() {
+    console.log('documento app cargado');
+    console.log(document)
+    const vmain = document.querySelector('main')
+
+
+    
+}
